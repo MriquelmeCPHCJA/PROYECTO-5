8a756ca8a466915a521783a35fd8375d5edb1b8b
Marcelo Riquelme <mriquelme@correo.com>
2024-12-21 22:48:48 -0300

    refactor: add home page and navbar

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,20 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
 import './index.css'
 import { RouterProvider } from 'react-router-dom'
 import { Router } from './Router'
+import { CssBaseline } from '@mui/material';
 // import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <CssBaseline />
     <RouterProvider router={Router} />
   </StrictMode>,
 )
